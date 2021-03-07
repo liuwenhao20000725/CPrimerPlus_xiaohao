3851cf6655788a627aa5f25768549e7130819063
liuwenhao <liuwenhao20000725@foxmail.com>
2021-03-07 20:38:49 +0800

    Initial commit

---
+++ Chapter1/Ch1.1.c
@@ -0,0 +1,11 @@
+////
+//// Created by xiaohao on 2021/3/7.
+////
+//#include <stdio.h>
+//int main(void ){
+//    int dogs;
+//    printf("How many dogs do you have?\n");
+//    scanf("%d",&dogs);
+//    printf("So you have %d dog(s)!\n",dogs);
+//    return 0;
+//}
---
+++ Chapter1/Ch1.2.c
@@ -0,0 +1,8 @@
+//
+// Created by xiaohao on 2021/3/7.
+//
+#include <stdio.h>
+int main(){
+    printf("Concrete contains gravel and cement.\n");
+    return 0;
+}
